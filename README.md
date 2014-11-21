c85478d53b0c48ecbbbe54b2d07d8b2d50c9f02f
hughrawlinson <hugh@codeoclock.net>
2014-11-21 03:01:51 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# GOAT - the Go Audio Toolkit
+
+Provides (at the moment, zero crossing rate). I'll start adding things as I need them while I'm learning go. Heads up: I have no idea if this works.
---
+++ goat.go
@@ -0,0 +1,15 @@
+package goat
+
+import (
+       'fmt'
+)
+
+func zcr (buffer []float) int{
+     int zcr = 0
+     for i := 0; i < len(buffer); i++ {
+     	 if (buffer[i] >= 0 && buffer[i+1] < 0) || (buffer[i] < 0 && buffer[i+1] >=0){
+	     zcr++
+	 }
+     }
+     return zcr
+}
